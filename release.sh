eeeda77908057695644f3c70c9fefd0cb544b673
Guillaume Savaton <senshuapash@gmail.com>
2010-09-26 19:20:51 +0200

    Added version file and script to automate the release build

---
+++ release.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SOURCE='sozi_edit_frame.inx  sozi_edit_frame.py  sozi_install.inx  sozi_install.py'
+TARGET=sozi-`cat VERSION`.zip
+
+rm -rf out
+./compress.sh
+
+for f in $SOURCE; do
+   cp $f out
+done
+
+cd out
+
+zip $TARGET *
+
