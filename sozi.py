4eb689dcb8afd5317a92c0052e58e110d063d496
senshu <senshuapash@gmail.com>
2011-03-02 19:25:47 +0100

    Closes #18, #24

--- sozi.py
+++ sozi.py
@@ -263,21 +263,20 @@ class Sozi(inkex.Effect):
       transition_zoom_field = self.create_spinbutton_field("transition-zoom-percent", "Zoom (%):", -100, 100)
       transition_profile_field = self.create_combo_field("transition-profile", "Profile:", Sozi.PROFILES)
 
-      self.fill_form()
-
       # Create save buttons
-      save_button = gtk.Button("Save frame")
-      save_button.set_sensitive(self.current_frame is not None)
-      save_button.connect("clicked", self.save_frame)
-      save_button.connect_object("clicked", gtk.Widget.destroy, window)
+      self.save_button = gtk.Button("Save frame")
+      self.save_button.connect("clicked", self.save_frame)
+      self.save_button.connect_object("clicked", gtk.Widget.destroy, window)
 
-      save_as_new_frame_button = gtk.Button("Save as new frame")
-      save_as_new_frame_button.connect("clicked", self.save_as_new_frame)
-      save_as_new_frame_button.connect_object("clicked", gtk.Widget.destroy, window)
+      self.save_as_new_frame_button = gtk.Button("Save as new frame")
+      self.save_as_new_frame_button.connect("clicked", self.save_as_new_frame)
+      self.save_as_new_frame_button.connect_object("clicked", gtk.Widget.destroy, window)
 
       buttons_box = gtk.HBox()
-      buttons_box.pack_start(save_button)
-      buttons_box.pack_start(save_as_new_frame_button)
+      buttons_box.pack_start(self.save_button)
+      buttons_box.pack_start(self.save_as_new_frame_button)
+
+      self.fill_form()
 
       # Create frame list
       frame_index = len(self.frames)
@@ -299,10 +298,7 @@ class Sozi(inkex.Effect):
 
          list_store.append([str(i+1) + ". " + title, color])
 
-      if frame_index < len(self.frames):
-         self.fields["sequence"].set_range(1, len(self.frames))
-      else:
-         self.fields["sequence"].set_range(1, len(self.frames) + 1)
+      self.fields["sequence"].set_range(1, len(self.frames) + 1)
       self.fields["sequence"].set_value(frame_index + 1)
 
       list_renderer = gtk.CellRendererText()
@@ -312,9 +308,13 @@ class Sozi(inkex.Effect):
       list_view = gtk.TreeView(list_store)
       list_view.append_column(list_column)
 
+      selection = list_view.get_selection()
       if frame_index < len(self.frames):
-         list_view.get_selection().select_path(frame_index)
+         selection.select_path(frame_index)
          list_view.scroll_to_cell(frame_index)
+
+      selection.set_mode(gtk.SELECTION_SINGLE) # TODO multiple selection
+      selection.connect("changed", self.on_selection_changed)
 
       list_scroll = gtk.ScrolledWindow()
       list_scroll.set_policy(gtk.POLICY_NEVER, gtk.POLICY_AUTOMATIC)	
@@ -359,10 +359,12 @@ class Sozi(inkex.Effect):
    def fill_form(self):
       # Rectangles are configured to be hidden by default.
       # Other elements are configured to be visible.
-      if self.current_frame is None:
+      if self.current_frame is not None:
+         tag = self.current_frame["svg_element"].tag
+      elif self.selected_element is not None:
          tag = self.selected_element.tag
       else:
-         tag = self.current_frame["svg_element"].tag
+         tag = None
 
       if tag == "rect" or tag == "{" + inkex.NSS["svg"] + "}rect":
          default_hide = "true"
@@ -370,7 +372,7 @@ class Sozi(inkex.Effect):
          default_hide = "false"
 
       self.fill_text_field("title", "New frame")
-      self.fill_spinbutton_field("sequence", 1)
+      self.fill_spinbutton_field("sequence", len(self.frames) + 1)
       self.fill_checkbox_field("hide", default_hide)
       self.fill_checkbox_field("clip", "true")
       self.fill_checkbox_field("timeout-enable", "false")
@@ -378,6 +380,9 @@ class Sozi(inkex.Effect):
       self.fill_spinbutton_field("transition-duration-ms", 1000)
       self.fill_spinbutton_field("transition-zoom-percent", 0)
       self.fill_combo_field("transition-profile", Sozi.PROFILES, 0)
+
+      self.save_button.set_sensitive(self.current_frame is not None)
+      self.save_as_new_frame_button.set_sensitive(self.current_frame is not None or self.selected_element is not None)
 
 
    def save_frame(self, widget):
@@ -419,6 +424,15 @@ class Sozi(inkex.Effect):
       }
       self.frames.append(self.current_frame)
       self.save_frame(widget)
+
+
+   def on_selection_changed(self, selection):
+      model, rows = selection.get_selected_rows()
+      if len(rows) > 0:
+         self.current_frame = self.frames[rows[0][0]]
+      else:
+         self.current_frame = None
+      self.fill_form()
 
 
    def destroy(self, widget, data=None):
