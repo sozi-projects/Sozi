6153bcbf930d8800aaac9f43b03b942a86966da7
senshu <senshuapash@gmail.com>
2011-05-16 22:20:00 +0200

    Switched to a dual MIT/GPL license

--- compress.sh
+++ compress.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Sozi - A presentation tool using the SVG standard
+#
+# Copyright (C) 2010-2011 Guillaume Savaton
+#
+# This program is dual licensed under the terms of the MIT license
+# or the GNU General Public License (GPL) version 3.
+# A copy of both licenses is provided in the doc/ folder of the
+# official release of Sozi.
+# 
+# See http://sozi.baierouge.fr/wiki/en:license for details.
+   
 JSMIN=`which jsmin`
 YUI1=`which yui-compressor`
 YUI2=`which yuicompressor`
