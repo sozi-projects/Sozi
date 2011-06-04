03fe2c39daf2d396c7f80f8e18b275d66ee5e632
senshu <senshuapash@gmail.com>
2011-06-04 21:07:33 +0200

    Indent = 4 spaces. Passes JSLint

--- player/animator.js
+++ player/animator.js
@@ -11,54 +11,48 @@
  * See http://sozi.baierouge.fr/wiki/en:license for details.
  */
 
-/*jslint plusplus: false, indent: 3, browser: true */
-/*global window: true */
-
 var sozi = sozi || {};
 
 sozi.Animator = function (timeStepMs, onStep, onDone) {
-   this.timeStepMs = timeStepMs || 40;
-   this.onStep = onStep;
-   this.onDone = onDone;
+    this.timeStepMs = timeStepMs || 40;
+    this.onStep = onStep;
+    this.onDone = onDone;
 
-   this.durationMs = 0;
-   this.data = {};
-   this.initialTime = 0;
-   this.started = false;
-   this.timer = 0;
+    this.durationMs = 0;
+    this.data = {};
+    this.initialTime = 0;
+    this.started = false;
+    this.timer = 0;
 };
 
 sozi.Animator.prototype.start = function (durationMs, data) {
-   this.durationMs = durationMs;
-   this.data = data;
+    this.durationMs = durationMs;
+    this.data = data;
 
-   this.initialTime = Date.now();
-   this.onStep(0, this.data);
+    this.initialTime = Date.now();
+    this.onStep(0, this.data);
 
-   if (!this.started) {
-      this.started = true;
-      this.timer = window.setInterval(this.step.bind(this), this.timeStepMs);
-   }
+    if (!this.started) {
+        this.started = true;
+        this.timer = window.setInterval(this.step.bind(this), this.timeStepMs);
+    }
 };
 
 sozi.Animator.prototype.stop = function () {
-   if (this.started) {
-      window.clearInterval(this.timer);
-      this.started = false;
-   }
+    if (this.started) {
+        window.clearInterval(this.timer);
+        this.started = false;
+    }
 };
 
 sozi.Animator.prototype.step = function () {
-   var elapsedTime = Date.now() - this.initialTime;
-   if (elapsedTime >= this.durationMs) {
-      this.stop();
-      this.onStep(1, this.data);
-      this.onDone();
-   }
-   else {
-      this.onStep(elapsedTime / this.durationMs, this.data);
-   }
+    var elapsedTime = Date.now() - this.initialTime;
+    if (elapsedTime >= this.durationMs) {
+        this.stop();
+        this.onStep(1, this.data);
+        this.onDone();
+    } else {
+        this.onStep(elapsedTime / this.durationMs, this.data);
+    }
 };
-
-// vim: sw=3
 
