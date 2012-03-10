abfc9bb6481bb84abaaf865da97c94b856d5e875
Quentin Bérard <quent57@yahoo.fr>
2012-03-10 21:00:41 +0100

    Fix number field factor, add duplicate button, fix tooltips, fix gui spacing

--- editors/inkscape/sozi.py
+++ editors/inkscape/sozi.py
@@ -201,17 +201,23 @@ class SoziSpinButtonField(SoziField):
     A wrapper for a GTK SpinButton mapped to a Sozi frame attribute.
     """
     
-    def __init__(self, parent, attr, label, min_value, max_value, default_value, digits=0, increments=1):
+    def __init__(self, parent, attr, label, min_value, max_value, default_value, factor=1, digits=0, increments=1):
         """
         Initialize a new spin button field.
             - label: label for the field
-            - min_value: the minimum integer value for the current field
-            - max_value: the maximum integer value for the current field
+            - min_value: the minimum float value for the current field
+            - max_value: the maximum float value for the current field
             - default_value: the default_value
+            - factor : eg: factor 1000 -> comboBox=1.3s ; sozi_svg=1300
             - decimals: number of decimals to display, eg: 2=> 1.00 or 0=> 1
             - increments: step between 2 number when clic left on a arrow.
         See class SoziField for other initializer arguments.
         """
+        factor = float(factor)
+        min_value = min_value * factor
+        max_value = max_value * factor
+        default_value = default_value * factor
+
         SoziField.__init__(self, parent, attr, label, gtk.HBox(), gtk.SpinButton(digits=digits), default_value)
         self.input_widget.set_range(min_value, max_value)
         # def set_increments(step, page)
@@ -221,14 +227,15 @@ class SoziSpinButtonField(SoziField):
         self.input_widget.set_numeric(True)
         self.container_widget.pack_start(gtk.Label(label))
         self.container_widget.pack_start(self.input_widget)
+        self.factor = factor
 
 
     def set_value(self, value):
-        self.input_widget.set_value(int(value))
+        self.input_widget.set_value(float(value) / self.factor)
 
 
     def get_value(self):
-        return unicode(self.input_widget.get_value_as_int())
+        return unicode(float(self.input_widget.get_value()) * self.factor)
 
 
     def on_focus_out(self, widget, event=None):
@@ -353,19 +360,21 @@ class SoziCreateAction(SoziAction):
             "Recreate frame " + new_frame_number)
         
         self.ui = ui
-        
-        # The new frame is a copy of the currently selected frame
-        self.index = ui.get_selected_index()
-        self.frame = ui.effect.create_new_frame(self.index)
-        for field in ui.fields.itervalues():
-            self.frame["frame_element"].set(field.ns_attr, field.get_value())
-                    
+                            
 
     def do(self):
         """
         Create a new frame and select it in the frame list.
         """
-        self.ui.effect.add_frame(self.frame)
+
+        self.ui.fill_form(None)
+        # The new frame is a copy of the currently selected frame
+        
+        frame = self.ui.effect.create_new_frame(None)
+        for field in self.ui.fields.itervalues():
+            frame["frame_element"].set(field.ns_attr, field.get_value())
+            
+        self.ui.effect.add_frame(frame)
         self.ui.append_frame_title(-1)
         self.ui.select_index(-1)
 
@@ -376,8 +385,7 @@ class SoziCreateAction(SoziAction):
         """
         self.ui.remove_last_frame_title()
         self.ui.effect.delete_frame(-1)
-        if self.index is not None:
-            self.ui.select_index(self.index)
+       
 
 
 class SoziDeleteAction(SoziAction):
@@ -420,6 +428,51 @@ class SoziDeleteAction(SoziAction):
         self.ui.insert_row(self.index, self.row)
 
 
+
+class SoziDuplicateAction(SoziAction):
+    """
+    A wrapper for a frame creation action.
+    """
+    
+    def __init__(self, ui):
+        """
+        Initialize a new frame creation action.
+            - ui: an instance of SoziUI
+        """
+        # The new frame will be added at the end of the presentation
+        new_frame_number = str(len(ui.effect.frames) + 1)
+
+        SoziAction.__init__(self,
+            "Remove frame " + new_frame_number,
+            "Recreate frame " + new_frame_number)
+        
+        self.ui = ui
+        
+        # The new frame is a copy of the currently selected frame
+        self.index = ui.get_selected_index()
+        self.frame = ui.effect.create_new_frame(self.index)
+        for field in ui.fields.itervalues():
+            self.frame["frame_element"].set(field.ns_attr, field.get_value())
+                    
+
+    def do(self):
+        """
+        Create a new frame and select it in the frame list.
+        """
+        self.ui.effect.add_frame(self.frame)
+        self.ui.append_frame_title(-1)
+        self.ui.select_index(-1)
+
+
+    def undo(self):        
+        """
+        Remove the created frame and select the previously selected frame.
+        """
+        self.ui.remove_last_frame_title()
+        self.ui.effect.delete_frame(-1)
+        if self.index is not None:
+            self.ui.select_index(self.index)
+    
 class SoziReorderAction(SoziAction):
     """
     A wrapper for a frame reordering action.
@@ -519,10 +572,11 @@ class SoziUI:
         self.window.connect("destroy", self.on_destroy)
         self.window.connect("key-press-event", self.on_key_press)
         self.window.set_title("Sozi")
-        self.window.set_icon_from_file(__file__+".svg")
+        self.window.set_icon_from_file(__file__ + ".svg")
+        self.window.set_border_width(5)
         
         # Enable icons on stock buttons
-        gtk.settings_get_default().set_long_property("gtk-button-images", True, "Sozi")
+        #gtk.settings_get_default().set_long_property("gtk-button-images", True, "Sozi")
 
         # Create fields for frame information
         self.fields = {
@@ -530,51 +584,59 @@ class SoziUI:
             "hide": SoziCheckButtonField(self, "hide", "Hide", "true"),
             "clip": SoziCheckButtonField(self, "clip", "Clip", "true"),
             "timeout-enable": SoziCheckButtonField(self, "timeout-enable", "Timeout enable", "false"),
-            "timeout-ms": SoziSpinButtonField(self, "timeout-ms", "Timeout (s)", 0, 3600, 5, digits=2, increments=0.5),
-            "transition-duration-ms": SoziSpinButtonField(self, "transition-duration-ms", "Duration (s)", 0, 3600, 1, digits=2, increments=0.5),
-            "transition-zoom-percent": SoziSpinButtonField(self, "transition-zoom-percent", "Zoom (%)", -100, 100, 0, digits=0, increments=5),
+            "timeout-ms": SoziSpinButtonField(self, "timeout-ms", "Timeout (s)", 0, 3600, 5, factor=1000, digits=2, increments=0.2),
+            "transition-duration-ms": SoziSpinButtonField(self, "transition-duration-ms", "Duration (s)", 0, 3600, 1, factor=1000, digits=2, increments=0.1),
+            "transition-zoom-percent": SoziSpinButtonField(self, "transition-zoom-percent", "Zoom (%)", -100, 100, 0, increments=5),
             "transition-profile": SoziComboField(self, "transition-profile", "Profile", SoziUI.PROFILES, SoziUI.PROFILES[0])
         }
 
 
         # Undo/redo widgets
-        self.undo_button = gtk.Button(stock=gtk.STOCK_UNDO)
+        self.undo_button = gtk.ToolButton(gtk.STOCK_UNDO)
         self.undo_button.set_sensitive(False)
+        self.undo_button.set_label("Annuler")
         self.undo_button.connect("clicked", self.on_undo)
 
-        self.redo_button = gtk.Button(stock=gtk.STOCK_REDO)
+        self.redo_button = gtk.ToolButton(gtk.STOCK_REDO)
         self.redo_button.set_sensitive(False)
         self.redo_button.connect("clicked", self.on_redo)
         
         toolBar = gtk.Toolbar()
-        toolBar.set_style(gtk.TOOLBAR_BOTH_HORIZ)
+        toolBar.set_style(gtk.TOOLBAR_BOTH)
         toolBar.add(self.undo_button)
         toolBar.add(self.redo_button)
        
 
         # Frame properties
-        frame_box = gtk.VBox()
+        frame_box = gtk.VBox(spacing=0)
         frame_box.pack_start(self.fields["title"].container_widget, expand=False)
         frame_box.pack_start(self.fields["hide"].container_widget, expand=False)
         frame_box.pack_start(self.fields["clip"].container_widget, expand=False)
         frame_box.pack_start(self.fields["timeout-enable"].container_widget, expand=False)
         frame_box.pack_start(self.fields["timeout-ms"].container_widget, expand=False)
 
-        frame_group = gtk.Frame("Frame")
+        frame_group = gtk.Frame()
+        # fixme, spaces are here for set width of list..
+        frame_label=gtk.Label("<b>Frame properties</b>              ")
+        frame_label.set_use_markup(True) # enable bold with <b>
+        frame_group.set_label_widget(frame_label)
         frame_group.add(frame_box)
         
         # Transition properties
-        transition_box = gtk.VBox()
+        transition_box = gtk.VBox(spacing=5)
         transition_box.pack_start(self.fields["transition-duration-ms"].container_widget, expand=False)
         transition_box.pack_start(self.fields["transition-zoom-percent"].container_widget, expand=False)
         transition_box.pack_start(self.fields["transition-profile"].container_widget, expand=False)
 
         transition_group = gtk.Frame("Transition")
+        transition_label=gtk.Label("<b>Transition</b>              ")
+        transition_label.set_use_markup(True) # enable bold with <b>
+        transition_group.set_label_widget(transition_label)
         transition_group.add(transition_box)
 
 
         # Fill right pane
-        right_pane = gtk.VBox()
+        right_pane = gtk.VBox(spacing=5)
         right_pane.pack_start(frame_group, expand=False)
         right_pane.pack_start(transition_group, expand=False)
 
@@ -599,19 +661,28 @@ class SoziUI:
 
         # Create new/delete buttons
         self.new_button = gtk.ToolButton()#stock=gtk.STOCK_NEW)
+        self.new_button.set_tooltip_text("Create a new frame using the selected element")
         self.new_button.set_stock_id(gtk.STOCK_ADD)        
         self.new_button.connect("clicked", self.on_create_new_frame)
 
         self.delete_button = gtk.ToolButton()#stock=gtk.STOCK_DELETE)
+        self.delete_button.set_tooltip_text("Delete the current frame")
         self.delete_button.set_stock_id(gtk.STOCK_REMOVE)
         self.delete_button.connect("clicked", self.on_delete_frame)
         
+        self.duplicate_button = gtk.ToolButton()#stock=gtk.STOCK_DELETE)
+        self.duplicate_button.set_tooltip_text("Duplicate the current frame")
+        self.duplicate_button.set_stock_id(gtk.STOCK_COPY)
+        self.duplicate_button.connect("clicked", self.on_duplicate_frame)
+
         # Create up/down buttons
         self.up_button = gtk.ToolButton()#stock=gtk.STOCK_GO_UP)
+        self.up_button.set_tooltip_text("Move up the current frame")
         self.up_button.set_stock_id(gtk.STOCK_GO_UP)
         self.up_button.connect("clicked", self.on_move_frame_up)
 
         self.down_button = gtk.ToolButton()#stock=gtk.STOCK_GO_DOWN)
+        self.up_button.set_tooltip_text("Move down the current frame")
         self.down_button.set_stock_id(gtk.STOCK_GO_DOWN)
         self.down_button.connect("clicked", self.on_move_frame_down)
 
@@ -619,20 +690,26 @@ class SoziUI:
         listToolBar.set_icon_size(1)
         listToolBar.add(self.new_button)
         listToolBar.add(self.delete_button)
+        listToolBar.add(self.duplicate_button)
         listToolBar.add(self.up_button)
         listToolBar.add(self.down_button)
 
         # Fill left pane
-        left_pane_content = gtk.VBox()
+        left_pane_content = gtk.VBox(spacing=0)
         left_pane_content.pack_start(list_scroll, expand=True, fill=True)
         left_pane_content.pack_end(listToolBar, expand=False)
 
-        list_group = gtk.Frame("List of frames     ")# fixme, width of list..
+        list_group = gtk.Frame()
+        # fixme, spaces are here for set width of list..
+        list_frame_label=gtk.Label("<b>List of frames</b>              ")
+        list_frame_label.set_use_markup(True) # enable bold with <b>
+        list_group.set_label_widget(list_frame_label)
         list_group.add(left_pane_content)
         
         left_pane = list_group
+        #left_pane.set_r
 
-        hbox = gtk.HBox()
+        hbox = gtk.HBox(spacing=5)
         hbox.pack_start(left_pane)
         hbox.pack_start(right_pane)
         
@@ -641,17 +718,16 @@ class SoziUI:
         cancel_button = gtk.Button(stock=gtk.STOCK_CANCEL)
         cancel_button.connect("clicked", self.on_full_undo)
         ok_button = gtk.Button(stock=gtk.STOCK_OK)#or Apply
-        ok_button.connect("clicked",gtk.main_quit)
+        ok_button.connect("clicked", gtk.main_quit)
         
-        buttonBar = gtk.HButtonBox()
-        buttonBar.add(cancel_button)
-        buttonBar.add(ok_button)
+        buttonBar = gtk.HBox(spacing=10)
+        buttonBar.pack_end(ok_button, False, False)
+        buttonBar.pack_end(cancel_button, False, False)
 
         statusBar = gtk.Statusbar()
-        statusBar.add(gtk.Label("ee"))
-        statusBar.set_tooltip_text("eeee")
+        #statusBar.push(statusBar.getContexteId("a"),"Etat initial")
         
-        vbox = gtk.VBox()
+        vbox = gtk.VBox(spacing=5)
         vbox.pack_start(toolBar)
         vbox.add(hbox)
         vbox.add(buttonBar)
@@ -660,26 +736,38 @@ class SoziUI:
         self.window.add(vbox)
         self.window.show_all()
 
+
+        self.select_is_new = False
+         
         # Get selected frame
         selected_frame = None
+        # if selection is already in Sozi, select it
         if len(effect.selected) > 0:
             for f in effect.frames:
                 if f["svg_element"].attrib["id"] in effect.selected:
                     selected_frame = f
                     break
-        elif len(effect.frames) > 0:
+        
+        # if selection is not already in Sozi, selection is new
+        if (selected_frame == None) and (len(effect.selected) > 0):
+            self.select_is_new = True
+                
+        if (selected_frame == None) and  len(effect.frames) > 0:
             selected_frame = effect.frames[0]
-
+#
         # Fill frame list
         for i in range(len(effect.frames)):
             self.append_frame_title(i)
-
+#
         if selected_frame is not None:
             index = effect.frames.index(selected_frame)
             self.list_view.get_selection().select_path((index,))
             self.list_view.scroll_to_cell(index)
         else:
             self.fill_form(None)
+        
+        if self.select_is_new==False:
+            self.new_button.set_sensitive(False)
         
         gtk.main()
         
@@ -763,20 +851,8 @@ class SoziUI:
         for field in self.fields.itervalues():
             field.set_with_frame(frame)
 
-        if frame is not None:
-            create_tooltip = "Duplicate the current frame"
-            delete_tooltip = "Delete the current frame"
-        else:
-            if len(self.effect.selected) > 0:
-                create_tooltip = "Create a new frame using the selected element"
-            else:
-                create_tooltip = "No element or frame selected"
-            delete_tooltip = "No frame selected"
-            
-        self.new_button.set_tooltip_text(create_tooltip)
-        self.new_button.set_sensitive(frame is not None or len(self.effect.selected) > 0)
+        self.duplicate_button.set_sensitive(frame is not None )
         
-        self.delete_button.set_tooltip_text(delete_tooltip)
         self.delete_button.set_sensitive(frame is not None)
 
 
@@ -824,6 +900,11 @@ class SoziUI:
         """
         self.do_action(SoziDeleteAction(self))
 
+    def on_duplicate_frame(self, widget):
+        """
+        
+        """
+        self.do_action(SoziDuplicateAction(self))
 
     def on_move_frame_up(self, widget):
         """
@@ -1013,7 +1094,6 @@ class Sozi(inkex.Effect):
         # Swap frames in frame list
         self.frames[first], self.frames[second] = self.frames[second], self.frames[first]
 
-
     def create_new_frame(self, index):
         """
         Create a new frame using the SVG element of the frame at the given index.
@@ -1021,13 +1101,13 @@ class Sozi(inkex.Effect):
         """
         if index is not None:
             svg_element = self.frames[index]["svg_element"]
-        else:            
+        else:
             svg_element = self.selected.values()[0]
             
         frame_element = inkex.etree.Element(inkex.addNS("frame", "sozi"))
         frame_element.set(inkex.addNS("refid", "sozi"), svg_element.attrib["id"]) # TODO check namespace?
-        frame_element.set(inkex.addNS("sequence", "sozi"), unicode(len(self.frames) + 1))
-        frame_element.set("id", self.uniqueId("frame" + unicode(len(self.frames) + 1)))
+        frame_element.set(inkex.addNS("sequence", "sozi"), unicode(len(self.frames)+1))
+        frame_element.set("id", self.uniqueId("frame" + unicode(len(self.frames)+1)))
         
         frame = {
             "frame_element": frame_element,
