068724aa91ac4993c3728faa007f97cb3adc99b2
senshu <senshuapash@gmail.com>
2012-03-16 21:26:43 +0100

    module.js passes JSLint

--- player/js/module.js
+++ player/js/module.js
@@ -1,8 +1,8 @@
 
 function module(path, body) {
-	var names = path.split(".");
-	var i;
-	var current = this;
+	var names = path.split("."),
+	    i,
+	    current = this;
 	for (i = 0; i < names.length; i += 1) {
 		if (typeof current[names[i]] === "undefined") {
 			current[names[i]] = {};
