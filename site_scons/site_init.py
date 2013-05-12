4498bd6985f6d71421b2568d22c0b4a751a898ab
senshu <senshuapash@gmail.com>
2013-05-12 22:37:28 +0200

    Use SCons to build a release bundle

--- site_scons/site_init.py
+++ site_scons/site_init.py
@@ -66,3 +66,42 @@ def UGLIFY_JS(env):
     bld = Builder(action = "uglifyjs --no-copyright --output $TARGET $SOURCE", suffix = ".js")
     env.Append(BUILDERS = { "UglifyJS": bld })
 
+
+#
+# Replacement for the Zip builder with relative path to root dir.
+# http://stackoverflow.com/questions/10793259/how-to-make-scons-not-include-the-base-dir-in-zip-files
+#
+
+import os.path
+import zipfile
+
+def zipbetter(target, source, env):
+    # Open the zip file with appending, so multiple calls will add more files
+    zf = zipfile.ZipFile(str(target[0]), 'a', zipfile.ZIP_DEFLATED)
+    for s in source:
+        # Find the path of the base file
+        basedir = os.path.dirname(str(s))
+        if s.isdir():
+            # If the source is a directory, walk through its files
+            for dirpath, dirnames, filenames in os.walk(str(s)):
+                for fname in filenames:
+                    path = os.path.join(dirpath, fname)
+                    if os.path.isfile(path):
+                        # If this is a file, write it with its relative path
+                        zf.write(path, os.path.relpath(path, basedir))
+        else:
+            # Otherwise, just write it to the file
+            flatname = os.path.basename(str(s))
+            zf.write(str(s), flatname)
+    zf.close()
+
+
+def ZIP_BETTER(env):
+    # Make a builder using the zipbetter function, that takes SCons files
+    zipbetter_bld = Builder(action = zipbetter,
+                            target_factory = SCons.Node.FS.default_fs.Entry,
+                            source_factory = SCons.Node.FS.default_fs.Entry)
+
+    # Add the builder to the environment
+    env.Append(BUILDERS = {'ZipBetter' : zipbetter_bld})
+
