46063de0e3b1f8ac39ad04a4a340cf8f275bcaaf
senshu <senshuapash@gmail.com>
2013-05-14 17:44:33 +0200

    Added CSS minifier

--- site_scons/site_tools/minifyjs.py
+++ site_scons/site_tools/minifyjs.py
@@ -1,12 +1,19 @@
 
 from SCons.Builder import Builder
+from SCons.Script import Copy
 
 
 def exists(env):
-    return env.WhereIs("uglifyjs")
+    return True
 
 
 def generate(env):
-    bld = Builder(action = "uglifyjs --no-copyright --output $TARGET $SOURCE", suffix = ".min.js", src_suffix = ".js")
+    if env.WhereIs("uglifyjs"):
+        action = "uglifyjs --no-copyright --output $TARGET $SOURCE"
+    elif env.WhereIs("yui-compressor"):
+        action = "yui-compressor -o $TARGET $SOURCE"
+    else:
+        action = Copy("$TARGET", "$SOURCE")
+    bld = Builder(action = action, suffix = ".min.js", src_suffix = ".js")
     env.Append(BUILDERS = { "MinifyJS": bld })
 
