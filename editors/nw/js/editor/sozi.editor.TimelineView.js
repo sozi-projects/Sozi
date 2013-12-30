df89c199622d21daedb9dab5568859240f69f408
senshu <guillaume@baierouge.fr>
2013-12-30 22:10:37 +0100

    UI to add layers to the timeline view

--- editors/nw/js/editor/sozi.editor.TimelineView.js
+++ editors/nw/js/editor/sozi.editor.TimelineView.js
@@ -1,38 +1,38 @@
 
 namespace("sozi.editor", function (exports) {
     "use strict";
-    
+
     exports.TimelineView = sozi.model.Object.create({
-    
+
         init: function (doc) {
             sozi.model.Object.init.call(this);
-            
+
             this.document = doc;
-            
+
             // Fill layer selector
-            var htmlLayerSelect = document.querySelector("#layer");
-            
+            var htmlLayerSelect = document.querySelector("#layer-select");
+
             for (var layerId in doc.layers) {
                 var layer = doc.layers[layerId];
                 if (!layer.auto) {
-                    var htmlOptionText = document.createTextNode(layer.label);
-
-                    var htmlOption = document.createElement("option");
-                    htmlOption.setAttribute("value", layerId);
-                    htmlOption.appendChild(htmlOptionText);
-
-                    htmlLayerSelect.appendChild(htmlOption);
+                    htmlLayerSelect.insertAdjacentHTML("beforeend", "<option value='" + layerId + "'>" + layer.label + "</option>");
                 }
             }
-            
+
             htmlLayerSelect.addEventListener("change", this.bind(this.onAddLayer), false);
-            
+
             return this;
         },
-        
+
         onAddLayer: function () {
-            // TODO remove layer from selector
-            console.log("Add layer: " + document.querySelector("#layer").value);
+            // Add row to the timeline for the selected layer
+            var htmlLayerSelect = document.querySelector("#layer-select");
+            var layerId = htmlLayerSelect.value;
+            var layerLabel = this.document.layers[layerId].label;
+            htmlLayerSelect.parentNode.parentNode.insertAdjacentHTML("beforebegin", "<tr id='timeline-" + layerId + "'><th>" + layerLabel + "</th></tr>");
+
+            // Remove layer from drop-down list
+            htmlLayerSelect.removeChild(htmlLayerSelect.querySelector("option[value='" + layerId + "']"));
         }
     });
 });
