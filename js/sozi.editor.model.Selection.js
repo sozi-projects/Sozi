5b04e66097810859d76a514d43ef052b22e512cf
senshu <guillaume.savaton@gmail.com>
2014-02-23 15:43:20 +0100

    Added source documentation

--- js/sozi.editor.model.Selection.js
+++ js/sozi.editor.model.Selection.js
@@ -1,7 +1,27 @@
 namespace("sozi.editor.model", function (exports) {
     "use strict";
 
+    /*
+     * The Selection object holds the currently selected
+     * frames and layers of the presentation.
+     *
+     * Events:
+     *  - changed: when the content of the selection has changed
+     */
     exports.Selection = sozi.model.Object.create({
+
+        /*
+         * Initialize a selection for a given presentation.
+         *
+         * The selection is initialized with the first frame
+         * and all layers.
+         *
+         * Parameters:
+         *  - pres: A Sozi presentation object
+         *
+         * Returns:
+         *  - The current selection object
+         */
         init: function (pres) {
             sozi.model.Object.init.call(this);
 
@@ -13,10 +33,34 @@ namespace("sozi.editor.model", function (exports) {
             return this;
         },
 
+        /*
+         * Check whether the given layer is selected.
+         *
+         * Parameters:
+         *  - layer: A layer object
+         *
+         * Returns:
+         *  - true if the layer is selected, else false
+         */
         hasLayer: function (layer) {
             return this.selectedLayers.indexOf(layer) >= 0;
         },
 
+        /*
+         * Add the given layer to the current selection.
+         *
+         * If the layer is already selected, this method
+         * has no effect.
+         *
+         * Parameters:
+         *  - layer: A layer object
+         *
+         * Fires:
+         *  - changed
+         *
+         * Returns:
+         *  - The current selection object
+         */
         addLayer: function (layer) {
             if (!this.hasLayer(layer)) {
                 this.selectedLayers.push(layer);
@@ -25,6 +69,21 @@ namespace("sozi.editor.model", function (exports) {
             return this;
         },
 
+        /*
+         * Remove the given layer from the current selection.
+         *
+         * If the layer does not belong to the selection,
+         * this method has no effect.
+         *
+         * Parameters:
+         *  - layer: A layer object
+         *
+         * Fires:
+         *  - changed
+         *
+         * Returns:
+         *  - The current selection object
+         */
         removeLayer: function (layer) {
             var layerIndex = this.selectedLayers.indexOf(layer);
             if (layerIndex >= 0) {
@@ -34,6 +93,21 @@ namespace("sozi.editor.model", function (exports) {
             return this;
         },
 
+        /*
+         * Toggle the status of the given layer in the current selection.
+         *
+         * If the layer is already selected, it is removed from the selection.
+         * If the layer is not already selected, it is added to the selection.
+         *
+         * Parameters:
+         *  - layer: A layer object
+         *
+         * Fires:
+         *  - changed
+         *
+         * Returns:
+         *  - The current selection object
+         */
         toggleLayer: function (layer) {
             if (this.hasLayer(layer)) {
                 this.removeLayer(layer);
@@ -44,20 +118,60 @@ namespace("sozi.editor.model", function (exports) {
             return this;
         },
 
+        /*
+         * Select the given layers.
+         *
+         * All layers from the given array are added to the selection.
+         * Previously selected layers that do not belong to the given
+         * array are removed from the current selection.
+         *
+         * Parameters:
+         *  - layers: an array of layer objects
+         *
+         * Fires:
+         *  - changed
+         *
+         * Returns:
+         *  - The current selection object
+         */
         selectLayers: function (layers) {
             this.selectedLayers = layers.slice();
             this.fire("changed");
             return this;
         },
 
+        /*
+         * Check whether the given frame is selected.
+         *
+         * Parameters:
+         *  - frame: a frame object
+         *
+         * Returns:
+         *  - true if the frame belongs to the current selection, else false
+         */
         hasFrame: function (frame) {
             return this.selectedFrames.indexOf(frame) >= 0;
         },
 
+        /*
+         * Check whether the given frame is the last selected frame.
+         *
+         * Parameters:
+         *  - frame: a frame object
+         *
+         * Returns:
+         *  - true if the given frame has been selected last, else false
+         */
         frameIsCurrent: function (frame) {
             return this.currentFrame === frame;
         },
 
+        /*
+         * Get the last selected frame.
+         *
+         * Returns:
+         *  - The frame that has been selected last, null if no frame is selected.
+         */
         get currentFrame() {
             if (this.selectedFrames.length) {
                 return this.selectedFrames[this.selectedFrames.length - 1];
@@ -65,6 +179,21 @@ namespace("sozi.editor.model", function (exports) {
             return null;
         },
 
+        /*
+         * Add the given frame to the current selection.
+         *
+         * If the frame is already selected, this method
+         * has no effect.
+         *
+         * Parameters:
+         *  - frame: A frame object
+         *
+         * Fires:
+         *  - changed
+         *
+         * Returns:
+         *  - The current selection object
+         */
         addFrame: function (frame) {
             if (!this.hasFrame(frame)) {
                 this.selectedFrames.push(frame);
@@ -73,6 +202,21 @@ namespace("sozi.editor.model", function (exports) {
             return this;
         },
 
+        /*
+         * Remove the given frame from the current selection.
+         *
+         * If the frame is not already selected, this method
+         * has no effect.
+         *
+         * Parameters:
+         *  - frame: A frame object
+         *
+         * Fires:
+         *  - changed
+         *
+         * Returns:
+         *  - The current selection object
+         */
         removeFrame: function (frame) {
             var frameIndex = this.selectedFrames.indexOf(frame);
             if (frameIndex >= 0) {
@@ -82,6 +226,21 @@ namespace("sozi.editor.model", function (exports) {
             return this;
         },
 
+        /*
+         * Toggle the status of the given frame in the current selection.
+         *
+         * If the frame is already selected, it is removed from the selection.
+         * If the frame is not already selected, it is added to the selection.
+         *
+         * Parameters:
+         *  - frame: A frame object
+         *
+         * Fires:
+         *  - changed
+         *
+         * Returns:
+         *  - The current selection object
+         */
         toggleFrame: function (frame) {
             if (this.hasFrame(frame)) {
                 this.removeFrame(frame);
@@ -93,16 +252,20 @@ namespace("sozi.editor.model", function (exports) {
         },
 
         /*
-         * Mark a frame as selected.
+         * Select the given frames.
+         *
+         * All frames from the given array are added to the selection.
+         * Previously selected frames that do not belong to the given
+         * array are removed from the current selection.
          *
          * Parameters:
-         *    - frame: The frame to select
+         *  - frames: an array of frame objects
          *
          * Fires:
-         *    - changed(array)
+         *  - changed
          *
          * Returns:
-         *    - The current object.
+         *  - The current selection object
          */
         selectFrames: function (frames) {
             this.selectedFrames = frames.slice();
@@ -110,9 +273,28 @@ namespace("sozi.editor.model", function (exports) {
             return this;
         },
 
+        /*
+         * Toggle the status of a sequence of frames in the current selection.
+         *
+         * If the selection is empty, then the given frame is selected.
+         * If the selection is not empty, the sequence starts at a frame
+         * next to the current frame and ends at the given frame:
+         *  - If the given frame comes after the current frame in the presentation order,
+         *    then the sequence starts at the frame after the current,
+         *  - otherwise, the sequence starts at the frame before the current.
+         *
+         * Parameters:
+         *  - frame: A frame object
+         *
+         * Fires:
+         *  - changed
+         *
+         * Returns:
+         *  - The current selection object
+         */
         toggleFramesTo: function (frame) {
             if (!this.selectedFrames.length) {
-                this.toggleFrame(frame);
+                this.addFrame(frame);
             }
             else {
                 var endIndex = frame.index;
--- js/sozi.editor.view.Timeline.js
+++ js/sozi.editor.view.Timeline.js
@@ -2,8 +2,35 @@
 namespace("sozi.editor.view", function (exports) {
     "use strict";
 
+    /*
+     * The Timeline view shows a table where columns represent frames
+     * and rows represent layers.
+     * The user can choose to manipulate only a subset of the layers of the document.
+     * The other layers are grouped into a "Default" row.
+     *
+     * The Timeline view allows the user to:
+     *  - choose editable layers from the document
+     *  - show/hide layers
+     *  - create/delete/reorder frames
+     *  - add/remove layers and frames to/from the selection
+     *
+     * See also:
+     *  - templates/sozi.editor.Timeline.html
+     */
     exports.Timeline = sozi.model.Object.create({
 
+        /*
+         * Initialize a Timeline view.
+         *
+         * The view is rendered and event handlers are set up.
+         *
+         * Parameters:
+         *  - pres: a presentation object
+         *  - selection: a selection object
+         *
+         * Returns:
+         *  - The current view
+         */
         init: function (pres, selection) {
             sozi.model.Object.init.call(this);
 
@@ -26,6 +53,17 @@ namespace("sozi.editor.view", function (exports) {
             return this;
         },
 
+        /*
+         * Action: Add a frame to the presentation.
+         *
+         * This method is called as a result of a user action
+         * in the current view.
+         *
+         * A new frame is added to the presentation after the
+         * currently selected frame (see Selection.currentFrame).
+         * If no frame is selected, the new frame is added at the
+         * end of the presentation.
+         */
         addFrame: function () {
             var index = this.selection.currentFrame ?
                 this.presentation.frames.indexOf(this.selection.currentFrame) + 1 :
@@ -33,41 +71,106 @@ namespace("sozi.editor.view", function (exports) {
             this.presentation.addFrame(sozi.editor.view.Preview.state, index);
         },
 
+        /*
+         * Event handler: A frame was added to the presentation.
+         *
+         * This method is called when a new frame has been added
+         * to the presentation (see Presentation event: frameAdded).
+         *
+         * The current view is updated and the selection is set to the
+         * new frame.
+         *
+         * Parameters:
+         *  - presentation: The presentation that fired the event
+         *  - frame: The new frame
+         */
         frameAdded: function (presentation, frame) {
             this.render();
             this.selection.selectFrames([frame]);
         },
 
+        /*
+         * Action: Add a layer to the current view.
+         *
+         * This method is called as a result of a user action
+         * in the current view.
+         *
+         * The layer at the given index is added to the current view
+         * and to the selection.
+         * The current view is updated.
+         *
+         * Parameters:
+         *  - layerIndex: The index of a layer in the presentation
+         */
         addLayer: function (layerIndex) {
             var layer = this.presentation.layers[layerIndex];
             this.editableLayers.push(layer);
             this.defaultLayers.splice(this.defaultLayers.indexOf(layer), 1);
             this.render();
             this.selection.addLayer(layer);
-            return this;
         },
 
+        /*
+         * Action: Remove a layer from the current view.
+         *
+         * This method is called as a result of a user action
+         * in the current view.
+         *
+         * The layer at the given index is removed from the current view
+         * and from the selection.
+         * The current view is updated.
+         *
+         * Parameters:
+         *  - layerIndex: The index of a layer in the presentation
+         */
         removeLayer: function (layerIndex) {
             var layer = this.presentation.layers[layerIndex];
             this.selection.removeLayer(layer);
             this.editableLayers.splice(this.editableLayers.indexOf(layer), 1);
             this.defaultLayers.push(layer);
             this.render();
-            return this;
         },
 
+        /*
+         * Check whether "default" layers are visible.
+         *
+         * Returns:
+         *  - true if at least one "default" layer is visible, else false
+         */
         get defaultLayersAreVisible() {
             return this.defaultLayers.some(function (layer) {
                 return layer.isVisible;
             });
         },
 
+        /*
+         * Check whether "default" layers are selected.
+         *
+         * Returns:
+         *  - true if all "default" layers are selected, else false
+         */
         get defaultLayersAreSelected() {
             return this.defaultLayers.every(function (layer) {
                 return this.selection.hasLayer(layer);
             }, this);
         },
 
+        /*
+         * Action: Update the selection for a given frame.
+         *
+         * This method is called as a result of a user action
+         * in the current view.
+         *
+         * The result of this action depends in the following
+         * modifier keys:
+         *  - Ctrl: toggle the selection status of the given frame
+         *  - Shift: toggle a sequence of frames to the given frame
+         *  - No modifier: set the selection to the given frame and all layers
+         *
+         * Parameters:
+         *  - evt: The DOM event of the user action
+         *  - frameIndex: The index of a frame in the presentation
+         */
         updateFrameSelection: function (evt, frameIndex) {
             var frame = this.presentation.frames[frameIndex];
             if (evt.ctrlKey) {
@@ -82,6 +185,22 @@ namespace("sozi.editor.view", function (exports) {
             }
         },
 
+        /*
+         * Action: Update the selection for a given layer.
+         *
+         * This method is called as a result of a user action
+         * in the current view.
+         *
+         * The result of this action depends in the following
+         * modifier keys:
+         *  - Ctrl: toggle the selection status of the given layer
+         *  - Shift: toggle a sequence of layers to the given layer
+         *  - No modifier: set the selection to the given layer and all frames
+         *
+         * Parameters:
+         *  - evt: The DOM event of the user action
+         *  - layerIndex: The index of a layer in the presentation
+         */
         updateLayerSelection: function (evt, layerIndex) {
             var layers = layerIndex >= 0 ?
                 [this.presentation.layers[layerIndex]] :
@@ -100,6 +219,25 @@ namespace("sozi.editor.view", function (exports) {
             }
         },
 
+        /*
+         * Action: Update the selection for a given layer and a given frame.
+         *
+         * This method is called as a result of a user action
+         * in the current view.
+         *
+         * The result of this action depends in the following
+         * modifier keys:
+         *  - Ctrl: toggle the selection status of the given layer and frame.
+         *          If both are selected, they are removed from the selection.
+         *          If at least one is not selected, they are added to the selection.
+         *  - Shift: toggle a sequence of layers and frames to the given layer and frame
+         *  - No modifier: set the selection to the given layer and frame
+         *
+         * Parameters:
+         *  - evt: The DOM event of the user action
+         *  - layerIndex: The index of a layer in the presentation
+         *  - frameIndex: The index of a frame in the presentation
+         */
         updateLayerAndFrameSelection: function (evt, layerIndex, frameIndex) {
             var layers = layerIndex >= 0 ?
                 [this.presentation.layers[layerIndex]] :
@@ -132,16 +270,41 @@ namespace("sozi.editor.view", function (exports) {
             }
         },
 
+        /*
+         * Action: Change the visibility of the given layer.
+         *
+         * This method is called as a result of a user action
+         * in the current view.
+         *
+         * Toggle the visibility of the given layer.
+         * If the layer becomes visible, it is added to the selection,
+         * otherwise, it is removed from the selection.
+         *
+         * Parameters:
+         *  - layerIndex: The index of a layer in the presentation
+         */
         updateLayerVisibility: function (layerIndex) {
             var layers = layerIndex >= 0 ?
                 [this.presentation.layers[layerIndex]] :
                 this.defaultLayers;
             layers.forEach(function (layer) {
                 layer.isVisible = !layer.isVisible;
-                this.selection.removeLayer(layer);
+                if (layer.isVisible) {
+                    this.selection.addLayer(layer);
+                }
+                else {
+                    this.selection.removeLayer(layer);
+                }
             }, this);
         },
 
+        /*
+         * Render the current view.
+         *
+         * The timeline is rendered with respect to the current state of
+         * the presentation and the selection.
+         * Event handlers are attached to UI elements.
+         */
         render: function () {
             var self = this;
 
