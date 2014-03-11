85a5342d4706465a99a0ce9b8a2217af34ba2b18
senshu <guillaume.savaton@gmail.com>
2014-03-12 00:00:42 +0100

    Build zip bundles

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks("grunt-nunjucks");
     grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks("grunt-node-webkit-builder");
+    grunt.loadNpmTasks("grunt-zip");
 
     var version = grunt.template.today("yy.mm.ddHHMM");
 
@@ -43,8 +44,31 @@ module.exports = function(grunt) {
                 "vendor/**/*",
                 "<%= nunjucks.templates.dest %>"
             ]
+        },
+
+        zip: {
+            win: {
+                cwd: "build/releases/Sozi/win",
+                src: "build/releases/Sozi/win/Sozi/**/*",
+                dest: "build/Sozi-win.zip"
+            },
+            mac: {
+                cwd: "build/releases/Sozi/mac",
+                src: "build/releases/Sozi/mac/Sozi.app/**/*",
+                dest: "build/Sozi-mac.zip"
+            },
+            linux32: {
+                cwd: "build/releases/Sozi/linux32",
+                src: "build/releases/Sozi/linux32/Sozi/**/*",
+                dest: "build/Sozi-linux32.zip"
+            },
+            linux64: {
+                cwd: "build/releases/Sozi/linux64",
+                src: "build/releases/Sozi/linux64/Sozi/**/*",
+                dest: "build/Sozi-linux64.zip"
+            }
         }
     });
 
-    grunt.registerTask("default", ["nunjucks", "nodewebkit"]);
+    grunt.registerTask("default", ["nunjucks", "nodewebkit", "zip"]);
 };
