e99583059c3e02e779e16d9c1802286a693e792e
senshu <guillaume.savaton@gmail.com>
2014-10-06 21:30:33 +0200

    Spilt main module into separate functions

--- js/sozi.editor.main.js
+++ js/sozi.editor.main.js
@@ -10,13 +10,59 @@ window.addEventListener("load", function () {
     
     var svgFileDescriptor;
 
-    function htmlGenerator(svgData) {
-        return function () {
+    function loadSVG(data) {
+        // Find the SVG root and check that the loaded document is valid SVG.
+        var svgRoot = $("#sozi-editor-view-preview").html(data).get(0).querySelector("svg");
+        if (svgRoot instanceof SVGSVGElement) {
+            presentation.init(svgRoot);
+            $("html head title").text(presentation.title);
+        }
+        else {
+            $.notify("Document is not valid SVG.", "error");
+        }
+    }
+
+    function loadJSON(backend, name, location) {
+        backend.find(name, location, function (fileDescriptor) {
+            if (fileDescriptor) {
+                backend.load(fileDescriptor);
+            }
+            else {
+                // If no JSON file is available, attempt to extract
+                // presentation data from the SVG document, assuming
+                // it has been generated from Sozi 13 or earlier.
+                // Then save the extracted data to a JSON file.
+                presentation.upgrade();
+
+                // Select the first frame
+                if (presentation.frames.length) {
+                    $.notify("Document was imported from Sozi 13 or earlier.", "success");
+                    selection.selectedFrames.push(presentation.frames.first);
+                }
+
+                backend.create(name, location, "application/json", JSON.stringify(presentation.toStorable()), function (fileDescriptor) {
+                    autosaveJSON(backend, fileDescriptor);
+                });
+            }
+        });
+    }
+
+    function autosaveJSON(backend, fileDescriptor) {
+        backend.autosave(fileDescriptor, function () {
+            return JSON.stringify(presentation.toStorable());
+        });
+    }
+
+    function createHTML(backend, name, location, data) {
+        function exportHTML() {
             return nunjucks.render("templates/sozi.export.html", {
-                svg: svgData,
+                svg: data,
                 pres: presentation
             });
-        };
+        }
+        backend.create(name, location, "text/html", exportHTML(), function (fileDescriptor) {
+            backend.autosave(fileDescriptor, exportHTML);
+        });
     }
 
     sozi.editor.backend.list.forEach(function (backend) {
@@ -27,72 +73,30 @@ window.addEventListener("load", function () {
                 var name = backend.getName(fileDescriptor);
                 var location = backend.getLocation(fileDescriptor);
 
-                if (/\.svg$/.test(name)) {
-                    if (!err) {
-                        // Find the SVG root and check that the loaded document is valid SVG.
-                        var svgRoot = $("#sozi-editor-view-preview").html(data).get(0).querySelector("svg");
-                        if (svgRoot instanceof SVGSVGElement) {
-                            svgFileDescriptor = fileDescriptor;
-                            presentation.init(svgRoot);
-                            $("html head title").text(presentation.title);
-                            
-                            var jsonName = name.replace(/\.svg$/, ".sozi.json");
+                if (err) {
+                    $.notify("File " + name + " could not be loaded.", "error");
+                }
+                else if (/\.svg$/.test(name)) {
+                    svgFileDescriptor = fileDescriptor;
 
-                            backend.find(jsonName, location, function (fileDescriptor) {
-                                if (fileDescriptor) {
-                                    backend.load(fileDescriptor);
-                                }
-                                else {
-                                    // If no JSON file is available, attempt to extract
-                                    // presentation data from the SVG document, assuming
-                                    // it has been generated from Sozi 13 or earlier.
-                                    // Then save the extracted data to a JSON file.
-                                    presentation.upgrade();
-                                    
-                                    // Select the first frame
-                                    if (presentation.frames.length) {
-                                        $.notify("Document was imported from Sozi 13 or earlier.", "success");
-                                        selection.selectedFrames.push(presentation.frames.first);
-                                    }
-                                    
-                                    backend.create(jsonName, location, "application/json", JSON.stringify(presentation.toStorable()), function (fileDescriptor) {
-                                        setupAutosave(backend, fileDescriptor);
-                                    });
-                                }
-                            });
+                    loadSVG(data);
+                    loadJSON(backend, name.replace(/\.svg$/, ".sozi.json"), location);
+                    createHTML(backend, name.replace(/\.svg$/, ".sozi.html"), location, data);
 
-                            // Always create an exported SVG file and set autosave
-                            var htmlName = name.replace(/\.svg$/, ".sozi.html");
-                            var htmlExporter = htmlGenerator(data);
-
-                            backend.create(htmlName, location, "text/html", htmlExporter(), function (fileDescriptor) {
-                                backend.autosave(fileDescriptor, htmlExporter);
-                            });
-
-                            backend.addListener("save", function (backend, fileDescriptor) {
-                                $.notify("Saved " + backend.getName(fileDescriptor), "info");
-                            });
-                        }
-                        else {
-                            $.notify("Document is not valid SVG.", "error");
-                        }
-                    }
-                    else {
-                        $.notify("File " + name + " could not be loaded.", "error");
-                    }
+                    backend.addListener("save", function (backend, fileDescriptor) {
+                        $.notify("Saved " + backend.getName(fileDescriptor), "info");
+                    });
                 }
                 else if (/\.sozi\.json$/.test(name)) {
-                    if (!err) {
-                        // Load presentation data from JSON file.
-                        presentation.fromStorable(JSON.parse(data));
+                    // Load presentation data from JSON file.
+                    presentation.fromStorable(JSON.parse(data));
 
-                        // Select the first frame
-                        if (presentation.frames.length) {
-                            selection.selectedFrames.push(presentation.frames.first);
-                        }
-
-                        setupAutosave(backend, fileDescriptor);
+                    // Select the first frame
+                    if (presentation.frames.length) {
+                        selection.selectedFrames.push(presentation.frames.first);
                     }
+
+                    autosaveJSON(backend, fileDescriptor);
                 }
             })
             .addListener("change", function (backend, fileDescriptor) {
@@ -103,10 +107,4 @@ window.addEventListener("load", function () {
             })
             .init(listItem);
     });
-
-    function setupAutosave(backend, fileDescriptor) {
-        backend.autosave(fileDescriptor, function () {
-            return JSON.stringify(presentation.toStorable());
-        });
-    }
 }, false);
