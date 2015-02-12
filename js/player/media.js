9495ac17620f19987d5f6cebb4f276f17ab64d2a
senshu <guillaume.savaton@gmail.com>
2015-02-12 17:14:08 +0100

    Back to strict and license blocks

--- js/player/media.js
+++ js/player/media.js
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+"use strict";
+
 var	svgNs = "http://www.w3.org/2000/svg";
 var soziNs = "http://sozi.baierouge.fr";
 var xhtmlNs = "http://www.w3.org/1999/xhtml";
