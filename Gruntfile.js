1e39b0b8c1ddcc5769ff926603517f80918e550d
senshu <guillaume@baierouge.fr>
2019-01-12 21:52:59 +0100

    Using Babel preset 'env' instead of 'es2015'

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,7 +93,7 @@ module.exports = function(grunt) {
          */
         babel: {
             options: {
-                presets: ["es2015"]
+                presets: ["env"]
             },
             all: {
                 files: [{
