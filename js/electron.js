13f9ee9e13a0a06566f2d7d8527f46844527d136
senshu <guillaume@baierouge.fr>
2019-08-05 17:11:29 +0200

    Upgraded to Electron 6

--- js/electron.js
+++ js/electron.js
@@ -1,5 +1,5 @@
 
-import {app, BrowserWindow} from "electron";
+import {app, BrowserWindow, Menu} from "electron";
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -9,13 +9,23 @@ function createWindow () {
     // This sets the initial window size.
     // If Sozi has been opened before, the size and location will be
     // loaded from local storage in backend/Electron.js.
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    // Electron 6 provides a default menu bar that cannot be removed.
+    mainWindow.setMenuBarVisibility(false);
+
     mainWindow.loadURL(`file://${__dirname}/../index.html`);
 
     // mainWindow.toggleDevTools();
 
     // Emitted when the window is closed.
-    mainWindow.on('closed', function () {
+    mainWindow.on("closed", function () {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
@@ -26,18 +36,18 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on("window-all-closed", function () {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
+    if (process.platform !== "darwin") {
         app.quit();
     }
 });
 
-app.on('activate', function () {
+app.on("activate", function () {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
