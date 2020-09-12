5c9f535f955261c0470cd818f21793920fb3d4f0
aumouvantsillage <guillaume@baierouge.fr>
2020-09-12 12:23:05 +0200

    Completed documentation in FrameList and FrameNumber

--- js/player/FrameNumber.js
+++ js/player/FrameNumber.js
@@ -2,10 +2,23 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/** @module */
+ /** Show the current frame number.
+  *
+  * This module is part of the Sozi player embedded in each presentation.
+  *
+  * @module
+  */
 
 import * as FrameList from "./FrameList";
 
+/** Initialize the frame number management.
+ *
+ * This function registers event handlers:
+ * - A {@linkcode module:player/Viewport.frameChange|frameChange} event from the Sozi player will update the frame number.
+ * - A `click` event on the frame number will open the frame list.
+ *
+ * @param {module:player/Player.Player} player - The current Sozi player.
+ */
 export function init(player) {
     const frameNumber = document.querySelector(".sozi-frame-number");
 
